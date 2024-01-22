7afcb4540d7094b9f47867e6ad5e4988c0390f7d
BT_1N3_33 <bthssample@bths.edu>
2024-01-22 09:28:21 -0500

    finished + final fix + comments + fixed spacing between calculation+ fixed null in static history

--- src/TransactionHistory.java
+++ src/TransactionHistory.java
@@ -1,5 +1,5 @@
 public class TransactionHistory {
-    public static String history;
+    public static String history = "";
     public TransactionHistory(){
     }
     public static void addHistory(String his){
